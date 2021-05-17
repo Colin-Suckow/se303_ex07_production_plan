dcbc80e30db491df82bb3402569a5893b9a6109b
Colin Suckow <csuckow99@gmail.com>
2021-05-16 21:45:07 -0700

    Add no producers section and shortfall test

--- test/province_test.rb
+++ test/province_test.rb
@@ -23,4 +23,21 @@ class ProvinceTest < Minitest::Test
     assert_equal(-6, @asia.shortfall)
     assert_equal(292, @asia.profit)
   end
+
+  describe "no producers" do
+    before do
+      data = {
+        name: "No producers",
+        producers: [],
+        demand: 30,
+        price: 20
+      }
+      @noProducers = Province.new(data)
+    end
+
+    it "shortfall" do
+      assert_equal(30, @noProducers.shortfall)
+    end
+  end
+
 end
